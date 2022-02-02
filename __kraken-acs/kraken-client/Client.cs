42babcee95ddd148a593a4ef5aaa7bc9ac5b8610
rincewound <aaargh@minddebugger.com>
2022-02-02 19:21:20 +0100

    Add reference for adaptor lib

---
+++ __kraken-acs/kraken-client/Client.cs
@@ -0,0 +1,33 @@
+﻿using EventStore.Client;
+
+namespace kraken_client
+{
+    public class Client
+    {
+        const string streamName = "kraken-events";
+
+        EventStoreClient client;
+        public Client(EventStoreClientSettings settings)
+        {
+            client = new EventStoreClient(settings);
+        }
+
+        public void pushEvent(kraken.events.Event e)
+        {
+            var theEvent = new EventData(
+                Uuid.NewUuid(),
+                "Event",
+                e.toUTF8()
+                );
+
+           var task = client.AppendToStreamAsync(streamName, StreamState.Any, new[] { theEvent });
+           task.Wait();
+        }
+
+        public EventStoreClient.ReadStreamResult streamEvents(ulong startPos )
+        {
+            StreamPosition s = new StreamPosition(startPos);
+            return client.ReadStreamAsync(Direction.Forwards, streamName, s, 10);
+        }
+    }
+}
---
+++ __kraken-acs/kraken/Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using EventStore.Client;
+//using kraken;
+//using kraken.model;
+//using kraken.serializer;
+
+//var model = new OrganisationalModel();
+//model.addGrid("foo");
+//model.addGrid("foo.bar");
+
+//var sg = model.getGrid("foo.bar");
+
+//Serializer.Serialize(model, "test.txt");
+
+//var model2 = Serializer.Derserialize("test.txt");
+
+//var sg2 = model2.getGrid("foo.bar");
+
+//var settings = EventStoreClientSettings.Create("esdb://127.0.0.1:2113?tls=false&keepAliveTimeout=10000&keepAliveInterval=10000");
+//var c = new kraken_client.Client(settings);
+
+//var events = c.streamEvents(0);
+
+//await events.ForEachAsync(e =>
+//{
+//    var evt = kraken.events.Event.fromUTF8(e.Event.Data.ToArray());
+//    evt.applyToModel(model2);
+//});
+
+using CommandLine;
+using EventStore.Client;
+
+class Program
+{
+    [Verb("CreateGrid", HelpText = "Create a new access grid, this can be a subgrid of anothjer grid")]
+    class createGridOptions
+    {
+        [Value(0)]
+        public string GridName { get; set; }
+    }
+
+    [Verb("AddUserToGrid", HelpText = "Adds a given user to a grid without any special rights")]
+    class addUserToGridOptions
+    {
+        [Value(0)]
+        public string GridName { get; set; }
+
+        [Value(1)]
+        public string UserName { get; set; }
+    }
+
+    [Verb("AddAccessRight", HelpText = "Adds an access right for a user to a grid")]
+    class addAccessRightsOptions
+    {
+        [Value(0)]
+        public string GridName { get; set; }
+
+        [Value(1)]
+        public string UserName { get; set; }
+
+        [Value(2)]
+        public string AccessRight { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        kraken.events.Event evt = new kraken.events.Event();
+        Parser.Default.ParseArguments<createGridOptions,
+                                      addUserToGridOptions,
+                                      addAccessRightsOptions>(args)
+                          .WithParsed<createGridOptions>(cgrid =>
+                          {
+                              evt.evt = kraken.events.EventType.CreateGrid;
+                              evt.data["grid"] = cgrid.GridName;
+                              Console.WriteLine($"Add access grid {cgrid.GridName}");
+                          })
+                          .WithParsed<addUserToGridOptions>(ad =>
+                         {
+                             evt.evt = kraken.events.EventType.AddUserToGrid;
+                             evt.data["grid"] = ad.GridName;
+                             evt.data["name"] = ad.UserName;
+                             Console.WriteLine($"Add user {ad.UserName} to access grid {ad.GridName}");
+                         })
+                          .WithParsed<addAccessRightsOptions>(ad =>
+                          {
+                              evt.evt = kraken.events.EventType.AddAccessRight;
+                              evt.data["grid"] = ad.GridName;
+                              evt.data["name"] = ad.UserName;
+                              evt.data["accessright"] = ad.AccessRight;
+                              Console.WriteLine($"Add accessright {ad.AccessRight} for user {ad.UserName} to access grid {ad.GridName}");
+                          });
+
+
+
+        var client = new kraken_client.Client(EventStoreClientSettings.Create("esdb://127.0.0.1:2113?tls=false&keepAliveTimeout=10000&keepAliveInterval=10000"));
+        client.pushEvent(evt);
+    }
+}
